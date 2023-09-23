fc1187678c34cf3fd847b1769c423a23e582f709
Dylan Erdini <erdini.dylan@gmail.com>
2023-09-23 20:22:26 -0300

    feat(users): add validateUniqueEmail middleware to check duplicate emails

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,4 +1,5 @@
 import { HTTP_STATUSES } from "../constants/http.js";
+import { users } from "../models/users.js"
 
 export const validateUserData = (req, res, next) => {
     const { username, name, lastName, email, password, passwordConf } = req.body
@@ -7,6 +8,14 @@ export const validateUserData = (req, res, next) => {
     }
     if ([username, name, lastName, email, password, passwordConf].some(element => element === "")) {
         return res.status(HTTP_STATUSES.BAD_REQUEST).json({ error: "The fields cannot be empty" })
+    }
+    next()
+}
+
+export const validateUniqueEmail = (req, res, next) => {
+    const { email } = req.body
+    if (users.some(user => user.email === email)) {
+        return res.status(HTTP_STATUSES.UNAUTHORIZED).json({ msg: "The email is already in use" })
     }
     next()
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { newUser } from "../controllers/users.js"
-import { validateUserData } from "../middlewares/users.js"
+import { validateUserData, validateUniqueEmail } from "../middlewares/users.js"
 
 const router = express.Router()
 
 router
-    .post("/", validateUserData, newUser)       //SIGN UP
+    .post("/", validateUserData, validateUniqueEmail, newUser)       //SIGN UP
 
 export default router
