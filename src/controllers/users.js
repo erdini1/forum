37e713c8e4bdd04c479f196302c9dccb8b2addb6
Dylan Erdini <erdini.dylan@gmail.com>
2023-09-24 11:23:13 -0300

    feat(users): complete register controller and start login controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,8 +1,23 @@
 import { HTTP_STATUSES } from "../constants/http.js"
 import { users } from "../models/users.js"
 
-export const newUser = (req, res) => {
+export const register = (req, res) => {
     const { username, name, lastName, email, password } = req.body
+    const indexLastUser = users[users.length - 1].id     //ver el problema aca
 
-    res.status(HTTP_STATUSES.CREATED).json({ msg: "POST from routes" })
+    users.push({
+        id: users.length > 0 ? indexLastUser + 1 : 0,
+        username,
+        name,
+        lastName,
+        email,
+        password
+    })
+    res.status(HTTP_STATUSES.CREATED).json({ msg: "User created successfully" })
+}
+
+export const login = (req, res) => {
+    return res.status(HTTP_STATUSES.OK).json({
+        msg: "Loggued in successfully",
+    })
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { newUser } from "../controllers/users.js"
+import { register, login } from "../controllers/users.js"
 import { validateUserData, validateUniqueData, validateUserPasswords } from "../middlewares/users.js"
 
 const router = express.Router()
 
 router
-    .post("/", validateUserData, validateUniqueData, validateUserPasswords, newUser)       //SIGN UP
+    .post("/", validateUserData, validateUniqueData, validateUserPasswords, register, login)       //SIGN UP
+    .post("/login", login)   //SIGN IN
 
 export default router
