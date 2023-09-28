088f7883e6648aa6f8a6cb3a313040e3912219d1
Dylan Erdini <erdini.dylan@gmail.com>
2023-09-28 12:09:52 -0300

    feat(comments): Add and complete newComment controller

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,5 +1,15 @@
+import { comments } from "../models/comments.js"
 
 
 export const newComment = (req, res) => {
-    return res.json({ msg: "From comments controller" })
+    const topic = req.topic
+    const user = req.user
+    const { comment } = req.body
+    comments.push({
+        id: comments.length > 0 ? comments[comments.length - 1].id + 1 : 0,
+        topicId: topic.id,
+        username: user.username,
+        comment
+    })
+    return res.json({ msg: "Comment added succesfully" })
 }
--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { newComment } from "../controllers/comments.js";
-import { isAuthenticated, validateTopicExistence } from "../middlewares/topics.js";
+import { isAuthenticated, validateTopicExistence, validateTopicOwnership } from "../middlewares/topics.js";
 import { validateCommentData } from "../middlewares/comments.js";
 
 const router = express.Router()
 
-router.post("/:idTopic", isAuthenticated, validateTopicExistence, validateCommentData, newComment)
+// get all comment from user
+router.post("/:idTopic", isAuthenticated, validateTopicExistence, validateCommentData, newComment)  //add new comment
+// modify comment
+// delete comment
+
 
 export default router
